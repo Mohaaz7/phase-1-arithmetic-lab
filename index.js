ad5d930f07f2f7148e3c1bbb6dbf7d4aef0f705d
mohaaz12 <mohaaz3004@gmail.com>
2025-06-08 15:04:49 +0300

    arth js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+// 1. Multiply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;  // 31 * 2 = 62
+
+// 2. Random number > 0
+const random = Math.floor(Math.random() * 100) + 1;  // random integer > 0
+
+// 3. Modulo
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;  // 16 % 6 = 4
+
+// 4. Max
+const max = Math.max(1, 5, 10, 15, 20);  // max = 20
